703a8b39a7258a75c7535c9e15f17f2997286b0b
Michael Huang <michaelh@casual.place>
2022-11-13 20:54:18 -0800

    Correct the tests

--- tests/cli.rs
+++ tests/cli.rs
@@ -5,7 +5,7 @@ use std::fs;
 
 type TestResult = Result<(), Box<dyn std::error::Error>>;
 
-const PRG: &str = "wcr";
+const PRG: &str = "rust_wc";
 const EMPTY: &str = "tests/inputs/empty.txt";
 const FOX: &str = "tests/inputs/fox.txt";
 const ATLAMAL: &str = "tests/inputs/atlamal.txt";
@@ -33,7 +33,7 @@ fn dies_chars_and_bytes() -> TestResult {
         .assert()
         .failure()
         .stderr(predicate::str::contains(
-            "The argument '--bytes' cannot be used with '--chars'",
+            "The argument '--chars' cannot be used with '--bytes'",
         ));
     Ok(())
 }
@@ -162,8 +162,7 @@ fn atlamal_bytes_lines() -> TestResult {
 #[test]
 fn atlamal_stdin() -> TestResult {
     let input = fs::read_to_string(ATLAMAL)?;
-    let expected =
-        fs::read_to_string("tests/expected/atlamal.txt.stdin.out")?;
+    let expected = fs::read_to_string("tests/expected/atlamal.txt.stdin.out")?;
     Command::cargo_bin(PRG)?
         .write_stdin(input)
         .assert()
